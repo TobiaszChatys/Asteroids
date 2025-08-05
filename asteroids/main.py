dfecd899b9cbe07f5f7a64ddb6d74587c6b2f670
tobiaszchays <Tobiaszchatys1@gmail.com>
2025-08-05 20:42:04 +0100

    Added Triangle to represent the player to the game enviroment

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,12 +1,17 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
+
+    # Centre position
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +19,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
