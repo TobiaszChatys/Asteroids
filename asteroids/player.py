4901d22ca6e3ac746b4f2e772c6bc23a1739b824
tobiaszchays <Tobiaszchatys1@gmail.com>
2025-08-06 12:29:49 +0100

    Added up and up controls so they player is able to move around in the game enviroment

--- asteroids/player.py
+++ asteroids/player.py
@@ -21,7 +21,7 @@ class Player(CircleShape):
         return [a, b, c]
 
     def rotate(self, dt):
-        self.rotation += PLATER_TURN_SPEED * dt
+        self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -30,4 +30,12 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
 
